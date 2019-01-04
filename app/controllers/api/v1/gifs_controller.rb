600e8d9ca9633d06a285e0d0c026beee2b994e83
Cesar Jolibois <diazcd85@gmail.com>
2019-01-04 09:57:07 -0700

    WIP
    forgot to place a description on the previous commit.
    * what dealing with vcr didn't let me connect
    * modified dark sky service to return only daily objects
    * WIP building the conn from the gifs controller

--- app/controllers/api/v1/gifs_controller.rb
+++ app/controllers/api/v1/gifs_controller.rb
@@ -3,7 +3,9 @@ class Api::V1::GifsController < ApplicationController
     coordinates = Coordinate.new(CityGeoService.new(params).get_coordinates)
     exclude = "currently,minutely,hourly,alerts,flags"
     daily_weather = DarkSkyService.new({coordinate: coordinates, exclude: exclude }).weather_info
-    gift = GiphyService.new(daily_weather)
-    require "pry"; binding.pry
+
+    conn = Faraday.new(url: "http://api.giphy.com/v1/gifs/search?q=weather&api_key=#{ENV['giphy_api_key']}&limit=8")
+
+    
   end
 end
