4ce2b74a50ac5f3d34bfe6a3e7daaaeb731708d7
Cesar Jolibois <diazcd85@gmail.com>
2019-01-03 22:07:44 -0700

    Comlete user story 2
    Created User table with secure password, uniq email, and a random generate api_key.
    
    create in the user controller logic to prevent the creation of a user in the database if the email exists or the password conformation don't match

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+require 'securerandom'
+
+class User < ApplicationRecord
+  validates_presence_of :email, :password
+  validates_uniqueness_of :email
+
+  has_secure_password
+
+  before_create :create_key
+
+  def create_key
+    self.api_key = SecureRandom.base64(30)
+  end
+end
