2bfde83d63c8198f23739a8a79d7918b82e263e2
Cesar Jolibois <diazcd85@gmail.com>
2019-01-05 15:31:41 -0700

    User story 3 finish
    * Create session controller
    * Decieded not to add the current user method on application controller.
    * tested the edge case when the credentials are incorrect
    __Note__ : ask the instructors if is necesary to add the current user method because the session is going the be controlled with the API key

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,10 +2,9 @@ class Api::V1::SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
       render json: UserSerializer.new(user), status: 200
     else
-      render json: "email or password incorrect", status: 400
+      render json: "email or password doesn't exists", status: 422
     end
   end
 end
--- spec/requests/api/v1/user_log_in_spec.rb
+++ spec/requests/api/v1/user_log_in_spec.rb
@@ -16,7 +16,20 @@ describe 'User log in' do
     resp = JSON.parse(response.body, symbolize_names: true)
 
     expect(resp[:data][:attributes][:api_key]).to eq(user.api_key)
+  end
 
-    expect(current_user).to eq(user)
+  scenario 'send incorrect credentials and receive API key' do
+    user = create(:user, email: "whatever@example.com", password: "password", api_key: "potato")
+
+    user_params =  {
+      "email": "theotherever@example.com",
+      "password": "wordpass"
+    }
+
+    post "/api/v1/sessions", params: user_params
+
+    expect(response.status).to eq(422)
+
+    expect(response.body).to eq("email or password doesn't exists")
   end
 end
